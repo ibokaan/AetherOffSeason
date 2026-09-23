9d1f3cd6c360ae30a6bca0b9c22e4e4cafff691e
Tardufrc0929 <tardubas71@gmail.com>
2026-09-24 07:38:10 +1200

    Robot.java yazıldı.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,19 +1,44 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-/**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
- * this project, you must also update the Main.java file in the project.
- */
-public class Robot extends TimedRobot {
-  
+public class Robot extends TimedRobot { // Ana robot sınıfı, robotun yaşam döngüsünü yönetir.
+    private Command autonomousCommand;
+    private RobotContainer robotContainer;
 
+    @Override // Robot başlatıldığında çağrılır.
+    public void robotInit() { 
+        robotContainer = new RobotContainer(); 
+    }
+
+    @Override // Her zaman çağrılır, robotun durumunu günceller.
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+
+    @Override // Otonom periyod başlatıldığında çağrılır.
+    public void autonomousInit() {
+        autonomousCommand = robotContainer.getAutonomousCommand();
+
+        if (autonomousCommand != null) { // Otonom komut varsa, onu başlat.
+            autonomousCommand.schedule();
+        }
+    }
+
+    @Override 
+    public void teleopInit() { // Teleoperated periyod başlatıldığında çağrılır.
+        if (autonomousCommand != null) { // Otonom komut çalışıyorsa, onu iptal et.
+            autonomousCommand.cancel();
+        }
+    }
+
+    @Override
+    public void teleopPeriodic() {}
+
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll(); // Test moduna geçerken tüm komutları iptal et.
+    }
 }
